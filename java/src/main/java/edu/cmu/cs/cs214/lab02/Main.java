f2eeb8656b5b7704cea4cdca0a00ab2fc946c542
yzhou442 <yzhou020404@gmail.com>
2024-09-06 01:08:19 -0400

    Finish encapsulation task for java.

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,20 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
-
+        Shape rectangle = new Rectangle(2, 3);
         Renderer renderer = new Renderer(rectangle);
 
+        Shape square = new Square(3);
+        Renderer renderer2 = new Renderer(square);
+
+        Shape circle = new Circle(3);
+        Renderer renderer3 = new Renderer(circle);
+
         renderer.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
